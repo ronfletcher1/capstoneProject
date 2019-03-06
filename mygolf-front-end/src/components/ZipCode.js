0477284216f33cf58c0b756413dd686b46560790
ronfletcher1 <ronfletcher1@me.com>
2019-03-06 10:42:49 -0500

    addded mygolfclubs, updated wind component

--- mygolf-front-end/src/components/ZipCode.js
+++ mygolf-front-end/src/components/ZipCode.js
@@ -1,28 +1,29 @@
 import React from 'react';
 
 
-function ZipCode(){
+function ZipCode () {
     console.log("yo yo")
 
-   windSearch(e){
-         console.log(e)
-         e.preventDefault();
-         console.log("form submitted");
-         const zipCode = document.getElementById("searchTerm").value;
-         const url = `http://api.openweathermap.org/data/2.5/weather?units=imperial&zip=${zipCode},us&appid=482c145ce8edf1d69ea5168f9d06460c`;
-         fetch (url)
-         .then ((response)=>{
-            return response.json();
-         })
-         .then((myJson)=>{
-             const results = myJson.results;
-             console.log(results)
-             this.setState({
-                 weatherData: results
-             })
-         })
-    }
+    // const windSearch = (e) => {
+    //     console.log(e)
+    //     e.preventDefault();
+    //     console.log("form submitted");
+    //     const zipCode = document.getElementById("searchTerm").value;
+    //     const url = `http://api.openweathermap.org/data/2.5/weather?units=imperial&zip=${zipCode},us&appid=482c145ce8edf1d69ea5168f9d06460c`;
+    //     fetch (url)
+    //     .then ((response)=>{
+    //        return response.json();
+    //     })
+    //     .then((myJson)=>{
+    //         const results = myJson.results;
+    //         console.log(results)
+    //         this.setState({
+    //             weatherData: results
+    //         });
+    //     });
+    // }
 
+    
         return (
             <div>
                 <form onSubmit={this.zipCode}>
@@ -31,8 +32,8 @@ function ZipCode(){
                     <button type="submit"className="btn"/>
                 </form>
             </div>
-            
-        )
+        );
+
 }
 
 
