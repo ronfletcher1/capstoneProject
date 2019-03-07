1948e932f8cffd8907e3106f890406875bdc3c82
DigitalCrafts <digitalcrafts@DigitalCraftss-MacBook-Air-2.local>
2019-03-07 10:57:11 -0500

    testing

--- mygolf-front-end/src/App.js
+++ mygolf-front-end/src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import './assets/devices.min.css';
 import { BrowserRouter as Router,Route } from 'react-router-dom';
-import Main from './components/Main';
-import Wind from './components/Wind';
+// import Main from './components/Main';
+// import Wind from './components/Wind';
 // import ZipCode from './components/ZipCode';
-import DistanceToHole from './components/DistanceToHole';
-import Navbar from './components/Navbar';
+// import DistanceToHole from './components/DistanceToHole';
+// import Navbar from './components/Navbar';
 // import video from './components/GolfCouse.mp4';
 
 class App extends Component {
